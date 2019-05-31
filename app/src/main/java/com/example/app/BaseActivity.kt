06a4d8b0a12c3b6fe6a1c671fb79f29cd17bf1bd
Beong9 <qudrnjs1503@naver.com>
2019-05-31 21:02:01 +0900

    create 'two file'

---
+++ app/src/main/java/com/example/app/BaseActivity.kt
@@ -0,0 +1,6 @@
+
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+  fun getRepository() = (application as ReminderApp).getRepository()
+}
---
+++ app/src/main/java/com/example/app/NewReminderActivity.kt
@@ -0,0 +1,68 @@
+﻿import android.app.Activity
+import android.os.Bundle
+import android.content.Context
+import android.view.View
+import android.widget.SeekBar
+import android.support.design.widget.Snackbar
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_new_reminder.*
+import kotlin.math.roundToInt
+
+class NewReminderActivity : BaseActivity(), OnMapReadyCallback {
+
+  private lateinit var map: GoogleMap
+
+  private var reminder = Reminder(latLng = null, radius = null, message = null)
+
+  private val radiusBarChangeListener = object : SeekBar.OnSeekBarChangeListener {
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+      updateRadiusWithProgress(progress)
+
+      showReminderUpdate()
+    }
+  }
+
+ private fun updateRadiusWithProgress(progress: Int) {
+    val radius = getRadius(progress)
+    reminder.radius = radius
+    radiusDescription.text = getString(R.string.radius_description, radius.roundToInt().toString())
+  }
+
+companion object {
+    private const val EXTRA_LAT_LNG = "EXTRA_LAT_LNG"
+    private const val EXTRA_ZOOM = "EXTRA_ZOOM"
+
+    fun newIntent(context: Context, latLng: LatLng, zoom: Float): Intent {
+      val intent = Intent(context, NewReminderActivity::class.java)
+      intent
+          .putExtra(EXTRA_LAT_LNG, latLng)
+          .putExtra(EXTRA_ZOOM, zoom)
+      return intent
+    }
+  }
+
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_new_reminder)
+
+    val mapFragment = supportFragmentManager
+        .findFragmentById(R.id.map) as SupportMapFragment
+    mapFragment.getMapAsync(this)
+
+    instructionTitle.visibility = View.GONE
+    instructionSubtitle.visibility = View.GONE
+    radiusBar.visibility = View.GONE
+    radiusDescription.visibility = View.GONE
+    message.visibility = View.GONE
+
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+  }
+
