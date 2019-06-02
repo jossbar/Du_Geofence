3f6bbc1ac0032df540ebb210abdb821a08fe1151
jossbar <whtngus857@naver.com>
2019-06-02 16:35:01 +0900

    now we can run but It's not working different xml

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -98,7 +98,6 @@ class MainActivity : BaseActivity(),  OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
     }
 }
