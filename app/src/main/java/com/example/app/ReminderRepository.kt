90eb07aef213ae235472a075a580971c7ac6d2d1
jossbar <whtngus857@naver.com>
2019-06-03 14:35:37 +0900

    complete geofence event

--- app/src/main/java/com/example/app/ReminderRepository.kt
+++ app/src/main/java/com/example/app/ReminderRepository.kt
@@ -1,7 +1,15 @@
 package com.example.app
 
+import android.Manifest
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
 import com.example.app.Reminder
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.google.gson.Gson
 
 class ReminderRepository(private val context: Context) {
@@ -13,21 +21,80 @@ class ReminderRepository(private val context: Context) {
 
     private val preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
     private val gson = Gson()
+    private val geofencingClient = LocationServices.getGeofencingClient(context)
+    private val geofencePendingIntent: PendingIntent by lazy {
+        val intent = Intent(context, GeofenceTransitionService::class.java)
+        PendingIntent.getService(
+                context,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+    }
 
     fun add(reminder: Reminder,
             success: () -> Unit,
             failure: (error: String) -> Unit) {
-        val list = getAll() + reminder
-        saveAll(list)
-        success()
+        // 1
+        val geofence = buildGeofence(reminder)
+        if (geofence != null
+                && ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // 2
+            geofencingClient
+                    .addGeofences(buildGeofencingRequest(geofence), geofencePendingIntent)
+                    .addOnSuccessListener {
+                        // 3
+                        saveAll(getAll() + reminder)
+                        success()
+                    }
+                    .addOnFailureListener {
+                        // 4
+                        failure(GeofenceErrorMessages.getErrorString(context, it))
+                    }
+        }
+    }
+
+    private fun buildGeofence(reminder: Reminder): Geofence? {
+        val latitude = reminder.latLng?.latitude
+        val longitude = reminder.latLng?.longitude
+        val radius = reminder.radius
+
+        if (latitude != null && longitude != null && radius != null) {
+            return Geofence.Builder()
+                    .setRequestId(reminder.id)
+                    .setCircularRegion(
+                            latitude,
+                            longitude,
+                            radius.toFloat()
+                    )
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                    .build()
+        }
+
+        return null
+    }
+
+    private fun buildGeofencingRequest(geofence: Geofence): GeofencingRequest {
+        return GeofencingRequest.Builder()
+                .setInitialTrigger(0)
+                .addGeofences(listOf(geofence))
+                .build()
     }
 
     fun remove(reminder: Reminder,
                success: () -> Unit,
                failure: (error: String) -> Unit) {
-        val list = getAll() - reminder
-        saveAll(list)
-        success()
+        geofencingClient
+                .removeGeofences(listOf(reminder.id))
+                .addOnSuccessListener {
+                    saveAll(getAll() - reminder)
+                    success()
+                }
+                .addOnFailureListener {
+                    failure(GeofenceErrorMessages.getErrorString(context, it))
+                }
     }
 
     private fun saveAll(list: List<Reminder>) {
